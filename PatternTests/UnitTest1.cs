ab57feaa4c127c6243bfe9cd7e00fe416d053752
Volker Ernst Wollmann <volkerwollmann@t-online.de>
2020-10-21 10:18:10 +0200

    unit Tests for patterns

---
+++ PatternTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BehaviourPatterns.Visitor;
+
+namespace PatternTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestVisitor()
+        {
+            // Visitor
+            Vistor visitor = new Vistor();
+            visitor.Main();
+        }
+    }
+}
