b33dec58d95f7c221d5274f691ad573152f35d48
Volker Ernst Wollmann <volkerwollmann@t-online.de>
2021-01-28 10:20:13 +0100

    rearange code

--- Structural/Composite.cs
+++ Structural/Composite.cs
@@ -9,37 +9,6 @@ namespace StrucutralPatterns.Composite
     /// https://www.dofactory.com/net/composite-design-pattern
     /// </summary>
 
-    public class CompositeExample
-    {
-        /// <summary>
-        /// Entry point into console application.
-        /// </summary>
-
-        public static void Composite()
-        {
-            // Create a tree structure
-            Composite root = new Composite("root");
-            root.Add(new Leaf("Leaf A"));
-            root.Add(new Leaf("Leaf B"));
-
-            Composite comp = new Composite("Composite X");
-            comp.Add(new Leaf("Leaf XA"));
-            comp.Add(new Leaf("Leaf XB"));
-
-            root.Add(comp);
-
-            root.Add(new Leaf("Leaf C"));
-
-            // Add and remove a leaf
-            Leaf leaf = new Leaf("Leaf D");
-            root.Add(leaf);
-            root.Remove(leaf);
-
-            // Recursively display tree
-            root.Display(1);
-        }
-    }
-
     /// <summary>
     /// The 'Component' abstract class
     /// </summary>
@@ -118,6 +87,36 @@ namespace StrucutralPatterns.Composite
         public override void Display(int depth)
         {
             Console.WriteLine(new String('-', depth) + name);
+        }
+    }
+    public class CompositeExample
+    {
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+
+        public static void Composite()
+        {
+            // Create a tree structure
+            Composite root = new Composite("root");
+            root.Add(new Leaf("Leaf A"));
+            root.Add(new Leaf("Leaf B"));
+
+            Composite comp = new Composite("Composite X");
+            comp.Add(new Leaf("Leaf XA"));
+            comp.Add(new Leaf("Leaf XB"));
+
+            root.Add(comp);
+
+            root.Add(new Leaf("Leaf C"));
+
+            // Add and remove a leaf
+            Leaf leaf = new Leaf("Leaf D");
+            root.Add(leaf);
+            root.Remove(leaf);
+
+            // Recursively display tree
+            root.Display(1);
         }
     }
 }
