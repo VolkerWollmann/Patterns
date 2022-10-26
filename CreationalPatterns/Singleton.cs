3f4f747dfeeee27b0443e876058be936a47d4eff
Volker Ernst Wollmann <volker@volker-wollmann.de>
2022-10-26 13:37:42 +0200

    Singleton thread saftey

--- CreationalPatterns/Singleton.cs
+++ CreationalPatterns/Singleton.cs
@@ -26,6 +26,8 @@ namespace Patterns.CreationalPatterns
             // which, however, doesn't work really well in multithreaded program.
             private static Singleton _instance;
 
+            private static readonly object _lock = new object();
+
             // This is the static method that controls the access to the singleton
             // instance. On the first run, it creates a singleton object and places
             // it into the static field. On subsequent runs, it returns the client
@@ -34,7 +36,14 @@ namespace Patterns.CreationalPatterns
             {
                 if (_instance == null)
                 {
-                    _instance = new Singleton();
+                    // thread safetyness
+                    lock (_lock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new Singleton();
+                        }
+                    }
                 }
                 return _instance;
             }
