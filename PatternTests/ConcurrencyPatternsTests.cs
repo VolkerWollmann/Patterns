3a067d5d063840364cd6da173a70162bcafbe633
Volker Ernst Wollmann <volker@volker-wollmann.de>
2023-11-06 08:07:57 +0100

    Producer Consumer Pattern

---
+++ PatternTests/ConcurrencyPatternsTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Patterns.ConcurrencyPatterns;
+
+namespace PatternTests
+{
+    [TestClass]
+    public class ConcurrencyPatternsTests
+    {
+        [TestMethod]
+        public void ProducersConsumers()
+        {
+            Patterns.ConcurrencyPatterns.ProducersConsumers.RunProducersConsumers();
+        }
+    }
+}
